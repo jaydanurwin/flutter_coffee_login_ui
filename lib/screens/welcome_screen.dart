c96babaec4d133b92dc01d60d8994478cca491e5
Jaydan Urwin <jaydan@jaydanurwin.com>
2021-02-02 22:40:52 -0700

    removed finished code

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -4,74 +4,8 @@ class WelcomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Theme.of(context).backgroundColor,
-      body: Column(
-        children: [
-          Expanded(
-            flex: 3,
-            child: Container(
-              decoration: BoxDecoration(
-                image: DecorationImage(
-                    image: AssetImage(
-                        './assets/images/jakub-dziubak-unsplash.jpg'),
-                    fit: BoxFit.cover),
-              ),
-            ),
-          ),
-          Expanded(
-            flex: 2,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(bottom: 120.0),
-                  child: Column(
-                    children: [
-                      Text(
-                        '👋 Hello we’re\n Basic Coffee Co.',
-                        style: Theme.of(context).textTheme.headline3,
-                        textAlign: TextAlign.center,
-                      ),
-                      SizedBox(height: 24.0),
-                      FittedBox(
-                        child: GestureDetector(
-                          onTap: () {
-                            Navigator.pushNamed(context, '/login-screen');
-                          },
-                          child: Container(
-                            padding: EdgeInsets.symmetric(
-                                horizontal: 16, vertical: 12.0),
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(50),
-                              color: Colors.white,
-                            ),
-                            child: Row(
-                              children: [
-                                Text(
-                                  'Get Started',
-                                  style: TextStyle(
-                                      color: Theme.of(context).primaryColor,
-                                      fontWeight: FontWeight.w600),
-                                ),
-                                SizedBox(
-                                  width: 10.0,
-                                ),
-                                Icon(
-                                  Icons.arrow_forward_ios_outlined,
-                                  color: Theme.of(context).primaryColor,
-                                ),
-                              ],
-                            ),
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ],
+      body: Center(
+        child: Text('Welcome Screen'),
       ),
     );
   }
